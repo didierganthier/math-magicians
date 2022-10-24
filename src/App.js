b85d4443b8e218ecc984cac50b1ae57ca7b20fce
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-24 17:38:54 -0400

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="bg-[#22272D] h-screen pl-4 pt-4">
-      <Calculator />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="bg-[#22272D] h-screen pl-4 pt-4">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
-export default App;
